44b8eecdf4098ead3ee2106f0975f61e7fa12da9
hayssam <massyah@gmail.com>
2017-02-21 21:35:40 +0100

    req

--- hitargetMVP/settings.py
+++ hitargetMVP/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = '!x)ed^v_9v^bx^us0ac*6fk(edxi(vplyd54++yg84ds@if4uo'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['100doutes.pythonanywhere.com']
 
 # Application definition
 
