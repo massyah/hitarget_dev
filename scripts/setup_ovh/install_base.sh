30af1775f8d627790349ef67b24cd9bd6bfff562
hayssam <massyah@gmail.com>
2017-04-06 00:50:16 +0200

    clearing requirements

--- scripts/setup_ovh/install_base.sh
+++ scripts/setup_ovh/install_base.sh
@@ -1,2 +1,11 @@
+sudo apt-get install zlib1g-dev
+sudo apt-get install openssl
+sudo apt-get install readline-common
+sudo apt-get install ncurses
+
 sudo apt-get install htop
 sudo apt-get install git
+sudo apt-get install postgresql
+sudo apt-get install python-psycopg2
+sudo apt-get install libpq-dev
+sudo apt-get install python-dev
