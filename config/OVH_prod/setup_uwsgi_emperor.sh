1d1c92dd2f0b5d1e86d81536adda0d574c9e64ba
hayssam <massyah@gmail.com>
2017-04-06 02:52:21 +0200

    OVHPROD config

---
+++ config/OVH_prod/setup_uwsgi_emperor.sh
@@ -0,0 +1,7 @@
+# create a directory for the vassals
+sudo mkdir /etc/uwsgi
+sudo mkdir /etc/uwsgi/vassals
+# symlink from the default config directory to your config file
+sudo ln -s /home/hayssam/hitarget_dev/config/OVH_prod/hitarget_uswgi.ini /etc/uwsgi/vassals/
+# run the emperor
+uwsgi --emperor /etc/uwsgi/vassals --uid www-data --gid www-data
