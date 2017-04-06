4f8824232c335656d565f3189259a30dfebcd7a0
hayssam <massyah@gmail.com>
2017-04-06 03:05:45 +0200

    updated demo path

--- config/OVH_prod/autostart_uwsgi_emperor.sh
+++ config/OVH_prod/autostart_uwsgi_emperor.sh
@@ -1,4 +1,4 @@
 # inject into /etc/rc.local
-# /usr/local/bin/uwsgi --emperor /etc/uwsgi/vassals --uid www-data --gid www-data --daemonize /var/log/uwsgi-emperor.log
+# /usr/local/bin/uwsgi --emperor /etc/uwsgi/vassals --uid hayssam --gid hayssam --daemonize /var/log/uwsgi-emperor.log
 
 sudo /etc/init.d/rc.local start
